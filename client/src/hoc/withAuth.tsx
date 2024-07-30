4de74cbe3591a62bcf05679f1f61c36dc23e08e2
janmesh kumar <janmesh799@gmail.com>
2024-07-30 22:12:27 +0530

    fix: type errors && install: sharp dependency for image optimization

--- client/src/hoc/withAuth.tsx
+++ client/src/hoc/withAuth.tsx
@@ -3,21 +3,22 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../store';
 
 const withAuth = (WrappedComponent: React.ComponentType) => {
-  return (props: any) => {
-    if (typeof window !== 'undefined') {
-      const router = useRouter();
-      const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
+  const HOC = (props: any) => {
+    const router = useRouter();
+    const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
 
-      if (!isAuthenticated) {
-        router.replace('/login');
-        return null;
-      }
-
-      return <WrappedComponent {...props} />;
+    // Check if we are in the browser environment and if authentication is required
+    if (typeof window !== 'undefined' && !isAuthenticated) {
+      router.replace('/login');
+      return null;
     }
 
-    return null;
+    return <WrappedComponent {...props} />;
   };
+
+  HOC.displayName = `withAuth(${WrappedComponent.displayName || WrappedComponent.name})`;
+
+  return HOC;
 };
 
 export default withAuth;
